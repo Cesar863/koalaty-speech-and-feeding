d1dd321d165f8302d610452ead7fa81556f00f0b
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2025-03-09 14:34:54 -0400

    Refactored nav bar

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -15,106 +15,104 @@ import {
 	Typography,
 } from '@mui/material';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LogoImage from '../../assets/images/mainLogo.png';
 
-export const DesktopNavbar = () => {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Stack
-						direction="row"
-						sx={{
-							justifyContent: 'space-between',
-							alignItems: 'center',
-							width: '100%',
-						}}
-						spacing={2}
-					>
-						<Box>
-							<IconButton
-								size="large"
-								edge="start"
-								color="inherit"
-								aria-label="menu"
-								sx={{ mr: 2 }}
-							>
-								<Image
-									src={LogoImage}
-									height={56}
-									width={56}
-									alt="Logo for Koalaty Speech and Feeding."
-								/>
-							</IconButton>
-						</Box>
-						<Typography variant="h6">
-							Koalaty Speech and Feeding Therapy Services
-						</Typography>
-						<Stack direction="row" spacing={1}>
-							<Typography variant="h6">Link 1</Typography>
-							<Typography variant="h6">Link 1</Typography>
-							<Typography variant="h6">Link 1</Typography>
-						</Stack>
-					</Stack>
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
-};
-
-export const MobileNavBar = () => {
+export const Navbar = () => {
 	const [open, setOpen] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
 
 	const toggleDrawer = (open: boolean) => () => {
 		setOpen(open);
 	};
 
-	const DrawerList = (
-		<Box sx={{ width: 215 }} role="presentation" onClick={toggleDrawer(false)}>
-			<List>
-				{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
-					<ListItem key={text} disablePadding>
-						<ListItemButton>
-							<ListItemText primary={text} />
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
-		</Box>
+	useEffect(() => {
+		const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
+
+		checkScreenSize();
+		window.addEventListener('resize', checkScreenSize);
+
+		return () => window.removeEventListener('resize', checkScreenSize);
+	}, []);
+
+	const linkItems = ['Inbox', 'Starred', 'Send email', 'Drafts'];
+
+	const MobileDrawerList = (
+		<List>
+			{linkItems.map((text) => (
+				<ListItem key={text} disablePadding>
+					<ListItemButton>
+						<ListItemText primary={text} />
+					</ListItemButton>
+				</ListItem>
+			))}
+		</List>
 	);
 
 	return (
-		<Box>
-			<AppBar>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
 				<Toolbar>
 					<Grid2
 						container
 						width="100%"
 						direction="row"
-						sx={{
-							justifyContent: 'space-around',
-							textAlign: 'right',
-							alignItems: 'center',
-						}}
+						textAlign={isMobile ? 'right' : 'left'}
 					>
-						<Grid2 size={7} sx={{ textAlign: 'right' }}>
+						<Grid2 size={{ xs: 7, md: 4 }}>
 							<IconButton size="large" edge="start" color="inherit">
 								<Image
 									src={LogoImage}
-									height={40}
-									width={40}
+									height={isMobile ? 40 : 56}
+									width={isMobile ? 40 : 56}
 									alt="Logo for Koalaty Speech and Feeding."
 								/>
 							</IconButton>
 						</Grid2>
-						<Grid2 size={5} sx={{ textAlign: 'right' }}>
-							<Button onClick={toggleDrawer(true)}>
-								<MenuIcon />
-							</Button>
-							<Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
-								{DrawerList}
-							</Drawer>
+						{!isMobile ? (
+							<Grid2 size={4} alignContent="center">
+								<Typography variant="h6" textAlign="center">
+									Koalaty Speech and Feeding Therapy Services
+								</Typography>
+							</Grid2>
+						) : (
+							<></>
+						)}
+						<Grid2
+							size={{ xs: 5, md: 4 }}
+							textAlign="right"
+							alignItems="center"
+							flexGrow={1}
+							display="flex"
+							justifyContent="flex-end"
+						>
+							{isMobile ? (
+								<>
+									<Button
+										onClick={toggleDrawer(true)}
+										sx={{ background: 'transparent' }}
+									>
+										<MenuIcon />
+									</Button>
+									<Drawer
+										anchor="right"
+										open={open}
+										onClose={toggleDrawer(false)}
+									>
+										<Box sx={{ width: 215 }} role="presentation">
+											{MobileDrawerList}
+										</Box>
+									</Drawer>
+								</>
+							) : (
+								<Stack direction="row" width="100%" justifyContent="flex-end">
+									{linkItems.map((text) => (
+										<Typography key={text} padding={2}>
+											{text}
+										</Typography>
+									))}
+								</Stack>
+							)}
 						</Grid2>
 					</Grid2>
 				</Toolbar>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
 'use client';
 import ClientWrapper from '@/app/ClientWrapper';
 import { CssBaseline } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { DesktopNavbar, MobileNavBar } from './Components/Navbar';
+import { Navbar } from './Components/Navbar';
 
 export default function Home() {
-	const [isMobile, setIsMobile] = useState(false);
-
-	useEffect(() => {
-		const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
-
-		checkScreenSize();
-		window.addEventListener('resize', checkScreenSize);
-
-		return () => window.removeEventListener('resize', checkScreenSize);
-	}, []);
 	return (
 		<ClientWrapper>
 			<CssBaseline />
-			{isMobile ? <MobileNavBar /> : <DesktopNavbar />}
+			<Navbar />
 		</ClientWrapper>
 	);
 }
