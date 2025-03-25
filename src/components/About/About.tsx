3dc48a5de79d9c4d5fb48bc24cc43b615db8d845
noel-pena <azulu88@hotmail.com>
2025-03-25 19:12:30 -0400

    refactored some of pageheader to be more reusable. WIP

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,17 +1,27 @@
 import PageHeader from '@/components/PageHeader/PageHeader';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 
 function About() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <Box
       sx={{
         display: 'flex',
-        justifyContent: 'center',
+        pl: isMobile ? 0 : 3,
+        justifyContent: isMobile ? 'center' : 'flex-start',
         alignItems: 'center',
         minHeight: '100%',
       }}
     >
-      <PageHeader title="About" subtitle="Lorem Ipsum" />
+      <PageHeader
+        h1Props={{ fontSize: '3rem' }}
+        textAlign={isMobile ? 'center' : 'start'}
+        title="Charity Bobo"
+        subtitle="Speech Language Pathologist"
+        description="Lorem ipsum odor amet, consectetuer adipiscing elit. Curabitur eget nulla tortor hac consequat sit pulvinar."
+      />
     </Box>
   );
 }
--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -3,39 +3,59 @@ import {
   ListItemText,
   Stack,
   Typography,
+  type TypographyProps,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
 
 export interface PageHeaderProps {
   bubbleText?: string;
-  title: string;
-  subtitle: string;
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  textAlign?: 'start' | 'center';
+  h1Props?: TypographyProps;
 }
 
 function PageHeader(props: PageHeaderProps) {
-  const { bubbleText, title, subtitle } = props;
+  const {
+    bubbleText,
+    title,
+    subtitle,
+    description,
+    textAlign = 'center',
+    h1Props,
+  } = props;
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
-    <Stack alignItems="center" gap={isMobile ? 2 : 3}>
+    <Stack alignItems={textAlign} gap={isMobile ? 2 : 3}>
       {bubbleText && (
         <Card variant="outlined" sx={{ width: 'fit-content', p: 1 }}>
-          <ListItemText sx={{ textAlign: 'center' }}>
-            <Typography variant={isMobile ? 'subtitle2' : 'subtitle1'}>
+          <ListItemText sx={{ textAlign: textAlign }}>
+            <Typography variant={isMobile ? 'subtitle1' : 'h6'}>
               {bubbleText}
             </Typography>
           </ListItemText>
         </Card>
       )}
-      <ListItemText sx={{ textAlign: 'center' }}>
-        <Typography variant={isMobile ? 'h2' : 'h1'}>{title}</Typography>
+      <ListItemText sx={{ textAlign: textAlign }}>
+        <Typography variant={isMobile ? 'h2' : 'h1'} {...h1Props}>
+          {title}
+        </Typography>
       </ListItemText>
-      <ListItemText sx={{ textAlign: 'center' }}>
-        <Typography variant={isMobile ? 'h6' : 'h5'}>{subtitle}</Typography>
+      <ListItemText sx={{ textAlign: textAlign }}>
+        <Typography variant={isMobile ? 'h5' : 'h4'}>{subtitle}</Typography>
       </ListItemText>
+      {description && (
+        <ListItemText sx={{ textAlign: textAlign }}>
+          <Typography variant={isMobile ? 'h6' : 'subtitle1'}>
+            {description}
+          </Typography>
+        </ListItemText>
+      )}
     </Stack>
   );
 }
